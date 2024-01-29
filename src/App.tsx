ace11f2f328a8f830db0afe97deb07355ff13d58
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-29 11:37:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import Box from "./components/Box"
+
+function App() {
+
+  const [val,setVal] = useState("");
+
+  return (
+    <div>
+      <Box label={"Search"} value={val} setter={setVal} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Box.tsx
@@ -0,0 +1,20 @@
+import { Dispatch, SetStateAction } from "react";
+
+
+type InputValType = string | number;
+
+const Box = <T extends InputValType>({ label, value, setter }: {
+    label: string,
+    value: T,
+    setter: Dispatch<SetStateAction<T>>
+}) => {
+    return (
+        <form>
+            <label htmlFor="">{label}</label>
+            <input type="text" value={value} onChange={(e) => setter(e.target.value as T)} />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+export default Box
